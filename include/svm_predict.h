767cb993e337b0b6e07ea57caa25e924a2d80e79
Nghia Nguyen Hoang <nghiapickup@users.noreply.github.com>
2015-09-02 00:43:18 +0700

    Create svm_predict.h

---
+++ include/svm_predict.h
@@ -0,0 +1,16 @@
+#ifndef SVM_PREDICT_H
+#define SVM_PREDICT_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+//Define Gobal variables
+
+//Define Methods
+extern void exit_with_help_predict();
+extern int main_svm_predict(int argc, char **argv);
+
+#endif // SVM_PREDICT_H

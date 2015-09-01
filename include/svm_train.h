d057ed382c67df7a60b5eafde0e4c51cb9f267b7
Nghia Nguyen Hoang <nghiapickup@users.noreply.github.com>
2015-09-02 00:43:47 +0700

    Create svm_train.h

---
+++ include/svm_train.h
@@ -0,0 +1,18 @@
+#ifndef SVM_TRAIN_H
+#define SVM_TRAIN_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include "svm.h"
+
+//Define Gobal variables
+
+//Methods define
+extern int main_svm_train(int argc, char **argv);
+extern void exit_with_help_train();
+
+#endif // SVM_TRAIN_H

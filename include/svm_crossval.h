e365eaeb4e290f6d036f53648c27d149beb0a436
Nghia Nguyen Hoang <nghiapickup@users.noreply.github.com>
2015-09-02 00:42:54 +0700

    Create svm_crossval.h

---
+++ include/svm_crossval.h
@@ -0,0 +1,18 @@
+#ifndef SVM_CROSSVAL_H
+#define SVM_CROSSVAL_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include "svm.h"
+
+//Define Gobal variables
+
+//Methods define
+extern int main_svm_crossval(int argc, char **argv);
+extern void exit_with_help_crossval();
+
+#endif // SVM_CROSSVAL_H
